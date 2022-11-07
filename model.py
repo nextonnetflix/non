2d975dc3f5ba7b3f8d654aa9d1e4f7bea0561628
nextonnetflix <117617853+nextonnetflix@users.noreply.github.com>
2022-11-08 00:08:11 +0530

    Update model.py

--- model.py
+++ model.py
@@ -3,12 +3,25 @@ import pickle
 import pandas as pd
 
 movies_data=pickle.load(open('moviesmodel.pkl','rb'))
+similarity=pickle.load(open('similarity.pkl','rb'))
 
 import difflib
 def recommend(movie_name):
 	list_of_all_titles = movies_data['title'].tolist()
 	find_close_match = difflib.get_close_matches(movie_name, list_of_all_titles)
-	return find_close_match
+	close_match = find_close_match[0]
+	index_of_the_movie = movies_data[movies_data.title == close_match]['index'].values[0]
+	similarity_score = list(enumerate(similarity[index_of_the_movie]))
+	sorted_similar_movies = sorted(similarity_score, key = lambda x:x[1], reverse = True)
+	i=0
+	movieslist=[]
+	for movie in sorted_similar_movies:
+		index = movie[0]
+		title_from_index = movies_data[movies_data.index==index]['title'].values[0]
+		if (i<30):
+			movieslist.append(title_from_index)
+			i+=1
+	return movieslist
 
 st.title('Movie Recommendation System') 
 
